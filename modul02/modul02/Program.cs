7122269b8812a16c48d5e899cb0789a29d5c86d0
Елеонора Димитрова <dimitrova.eleonora@gmail.com>
2015-11-22 12:19:41 +0200

    Модул 02, Видео 01 - Добавяне на коментари

---
+++ modul02/modul02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace modul02
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//дефиниране на две числа
+
+			//събиране на две числа
+
+			//показване на резултата
+
+		}
+	}
+}
